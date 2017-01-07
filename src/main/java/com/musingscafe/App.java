f91affa3cd9934ecbcf5cc3e41e7f1216492353f
ayadav <ayadav@expedia.com>
2017-01-07 10:54:15 +0530

    initial checkin

---
+++ src/main/java/com/musingscafe/App.java
@@ -0,0 +1,9 @@
+package com.musingscafe;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Do not run me :). Instead run App.java in individual packages. I can be deleted :(" );
+    }
+}

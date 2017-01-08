83ece9b6d3fe718c91b9a5eb0d47fb2a2a7b08e0
ayadav <ayadav@expedia.com>
2017-01-08 23:06:11 +0530

    simple thread implementation and use of wait and notify

--- src/main/java/com/musingscafe/create/App.java
+++ src/main/java/com/musingscafe/create/App.java
@@ -1,7 +1,52 @@
 package com.musingscafe.create;
 
+import com.musingscafe.Utils;
+
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        SimpleThread simpleThread = new SimpleThread();
+
+        SimpleRunnable simpleRunnable = new SimpleRunnable();
+        Thread runnableThread = new Thread(simpleRunnable);
+
+        Thread anonymousThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                IntStream.range(0, 10)
+                        .forEach(i -> System.out.println("AnonymousThread says :- " + i));
+            }
+        });
+
+        Thread lambdaThread = new Thread(() -> IntStream.range(0, 10)
+                .forEach(i -> System.out.println("LambdaThread says :- " + i)));
+
+        simpleThread.start();
+        runnableThread.start();
+        anonymousThread.start();
+        lambdaThread.start();
+
+        Utils.sleep(100);
+        System.out.println("main ends");
+    }
+}
+
+class SimpleThread extends Thread {
+
+    @Override
+    public void run() {
+        IntStream.range(0, 10)
+                .forEach(i -> System.out.println("Hello from SimpleThread :- " + i));
+    }
+}
+
+class SimpleRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        IntStream.range(0, 10)
+                .forEach(i -> System.out.println("Hello from SimpleRunnable :- " + i));
     }
 }
---
+++ src/main/java/com/musingscafe/sync/App.java
@@ -0,0 +1,47 @@
+package com.musingscafe.sync;
+
+import com.musingscafe.Utils;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Processor processor = new Processor();
+        Thread firstThread = new Thread(() -> processor.produce());
+        Thread secondThread = new Thread(() -> processor.consume());
+
+        firstThread.start();
+        secondThread.start();
+
+        firstThread.join();
+        secondThread.join();
+    }
+}
+
+class Processor {
+
+    public void produce() {
+        System.out.println("In produce...");
+        synchronized (this) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("finishing produce...");
+    }
+
+    public void consume() {
+        Utils.sleep(100);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter to resume...");
+        scanner.nextLine();
+
+        synchronized (this) {
+            notify();
+        }
+    }
+}
